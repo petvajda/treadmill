be1075d6224c8f3863ec615d2d8cda0db1d3f829
Peter Vajda <petvajda@gmail.com>
2018-08-25 14:33:01 +0100

    Add more tests and some update

--- src/solfege.js
+++ src/solfege.js
@@ -6,13 +6,18 @@ const NAMES_MODES = [
 	["V",   "Mixolydian"],
 	["VI",  "Aeolian"],
 	["VII", "Locrian"]
-]
+];
+
+const CMajScale =  ["C", "D", "E", "F", "G", "A", "B"];
+const flatKeys  =  ["F", "Bb", "Eb", "Ab", "Db", "Gb", "Cb"];
+const sharpKeys  = ["G", "D", "A", "E", "B", "F#", "C#"];
 
 class Scale {
   constructor(base, mode) {
   	this.roman = NAMES_MODES[mode-1][0];
   	this.name  = NAMES_MODES[mode-1][1];
   	this.base  = base;
+		this.notes = CMajScale;
   }
 }
 
--- src/solfege.test.js
+++ src/solfege.test.js
@@ -1,21 +1,26 @@
 const Scale = require('./solfege.js');
 
-test('C Major scale name to equal Ionian', () => {
+test('C Major 1st degree scale name to equal Ionian', () => {
   let cmaj = new Scale("C", 1);
   expect(cmaj.name).toBe("Ionian");
 });
 
-test('C Major scale roman name to equal I', () => {
+test('C Major 1st degree scale roman name to equal I', () => {
   var cmaj = new Scale("C", 1);
   expect(cmaj.roman).toBe("I");
 });
 
-test('H Locrian scale name to equal Locrian', () => {
+test('C Major 7th degree scale name to equal Locrian', () => {
   var hloc = new Scale("H", 7);
   expect(hloc.name).toBe("Locrian");
 });
 
-test('H Locrian scale roman name to equal Ionian', () => {
+test('C Major 7th degree scale roman name to equal VII', () => {
   var hloc = new Scale("H", 7);
   expect(hloc.roman).toBe("VII");
+});
+
+test('C Major scale notes', () => {
+  let cmaj = new Scale("C", 1);
+  expect(cmaj.notes).toEqual(["C", "D", "E", "F", "G", "A", "B"]);
 });
