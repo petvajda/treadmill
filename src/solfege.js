a8f8112e5b83c1f1d2ffd64628360c69019b720a
Peter Vajda <petvajda@gmail.com>
2018-08-25 17:02:57 +0100

    Generate modes for C based scales

--- src/solfege.js
+++ src/solfege.js
@@ -8,16 +8,18 @@ const NAMES_MODES = [
 	["VII", "Locrian"]
 ];
 
-const CMajScale =  ["C", "D", "E", "F", "G", "A", "B"];
-const flatKeys  =  ["F", "Bb", "Eb", "Ab", "Db", "Gb", "Cb"];
-const sharpKeys  = ["G", "D", "A", "E", "B", "F#", "C#"];
+const CMajScale  = ["C",  "D",  "E",  "F",  "G",  "A",  "B" ];
+const flatKeys   = ["F",  "Bb", "Eb", "Ab", "Db", "Gb", "Cb"];
+const flatNotes  = ["Bb", "Eb", "Ab", "Db", "Gb", "Cb", "Fb"];
+const sharpKeys  = ["G",  "D",  "A",  "E",  "B",  "F#", "C#"];
+const sharpNotes = ["F#", "C#", "G#", "D#", "A#", "E#", "B#"];
 
 class Scale {
   constructor(base, mode) {
   	this.roman = NAMES_MODES[mode-1][0];
   	this.name  = NAMES_MODES[mode-1][1];
   	this.base  = base;
-		this.notes = CMajScale;
+		this.notes = CMajScale.slice(mode-1).concat(CMajScale.slice(0, mode-1));
   }
 }
 
--- src/solfege.test.js
+++ src/solfege.test.js
@@ -24,3 +24,33 @@ test('C Major scale notes', () => {
   let cmaj = new Scale("C", 1);
   expect(cmaj.notes).toEqual(["C", "D", "E", "F", "G", "A", "B"]);
 });
+
+test('D Dorian scale notes', () => {
+  let cmaj = new Scale("C", 2);
+  expect(cmaj.notes).toEqual(["D", "E", "F", "G", "A", "B", "C"]);
+});
+
+test('E Phrygian scale notes', () => {
+  let cmaj = new Scale("C", 3);
+  expect(cmaj.notes).toEqual(["E", "F", "G", "A", "B", "C", "D"]);
+});
+
+test('F Lydian scale notes', () => {
+  let cmaj = new Scale("C", 4);
+  expect(cmaj.notes).toEqual(["F", "G", "A", "B", "C", "D", "E"]);
+});
+
+test('G Mixolydian scale notes', () => {
+  let cmaj = new Scale("C", 5);
+  expect(cmaj.notes).toEqual(["G", "A", "B", "C", "D", "E", "F"]);
+});
+
+test('A Aeolian scale notes', () => {
+  let cmaj = new Scale("C", 6);
+  expect(cmaj.notes).toEqual(["A", "B", "C", "D", "E", "F", "G"]);
+});
+
+test('B Locrian scale notes', () => {
+  let cmaj = new Scale("C", 7);
+  expect(cmaj.notes).toEqual(["B", "C", "D", "E", "F", "G", "A"]);
+});
