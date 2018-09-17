0f2b10e60b1b4ca5ea83fbbcc62d925f404e5b19
Peter Vajda <petvajda@gmail.com>
2018-09-17 15:26:27 +0100

    Add a test page for rhythm

--- src/rhythm.js
+++ src/rhythm.js
@@ -1,15 +1,35 @@
-// var exports = module.exports = {};
 
-//exports.pattern = (noteValue=8, noteCount=4) => {
-const pattern = (noteValue=8, noteCount=4) => {
-  var notesString = "tabstave notation=true time=4/4 tablature=false\n";
-  notesString = notesString + "notes :"+ noteValue + " ";
+VF = Vex.Flow;
+
+const pattern = (noteValue=8) => {
+  var notes = [];
   for (var i = 0; i < noteValue ; i++ ) {
     if (Math.random() >= 0.5) {
-      notesString = notesString + "B/4 ";
+      noteString = noteValue.toString();
     } else {
-      notesString = notesString +  "## ";
+      noteString = noteValue.toString()+ "r";
     }
+    var note = new VF.StaveNote({keys: ["b/4"], duration: noteString });
+    notes.push(note);
   }
-  return notesString;
+  return notes;
 };
+
+const setUp = () => {
+  var div = document.getElementById("notes")
+  var renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
+  renderer.resize(500, 500);
+  var context = renderer.getContext();
+  context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
+  var stave = new VF.Stave(10, 40, 400);
+  stave.addClef("bass").addTimeSignature("4/4");
+  stave.setContext(context).draw();
+  var duration = 8;
+  var notes = pattern(duration);
+  var voice = new VF.Voice({num_beats: duration,  beat_value: duration});
+  voice.addTickables(notes);
+  var formatter = new VF.Formatter().joinVoices([voice]).format([voice], 300);
+  voice.draw(context, stave);
+};
+
+setUp();
