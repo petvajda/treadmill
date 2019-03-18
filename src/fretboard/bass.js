664ea715f702a0b35e7181569f5580431259cb7f
Peter Vajda <petvajda@gmail.com>
2019-03-18 10:19:17 +0000

    Start using svg.js

--- src/fretboard/bass.js
+++ src/fretboard/bass.js
@@ -1,41 +1,35 @@
-function draw() {
-  var canvas = document.getElementById('canvas');
-  if (canvas.getContext) {
-    var ctx = canvas.getContext('2d');
-    ctx.beginPath();
+SVG.on(document, 'DOMContentLoaded', function() {
+  var draw = SVG('drawing')
+  var numberOfStrings = 4;
+  var numberOfFrets = 24;
+  var numberOfSingleDots = 4;
+  var numberOfOctaves = 2;
 
-    var numberOfStrings = 4;
-    var numberOfFrets = 25;
-    var numberOfSingleDots = 4;
-    var numberOfOctaves = 2;
+  draw.font({ fill: '#003333', family: 'arial', size: '12px' })
+  draw.text('G').move(0, 20);
+  draw.text('D').move(0, 40);
+  draw.text('A').move(0, 60);
+  draw.text('E').move(0, 80);
 
-    ctx.font = '12px arial';
-    ctx.fillText('G', 5, 30);
-    ctx.fillText('D', 5, 50);
-    ctx.fillText('A', 5, 70);
-    ctx.fillText('E', 5, 90);
-
-    for (var i = 0; i < numberOfStrings; i++) {
-      ctx.fillRect(20, 25 + i * 20, 480, 1);
-    }
-
-    for (var i = 0; i < numberOfFrets; i++) {
-      ctx.fillRect(20 + i * 20, 25, 1, 60);
-    }
-
-    var offset = 0;
-    for (var o = 0; o < numberOfOctaves; o++) {
-      ctx.globalAlpha = 0.65;
-      ctx.beginPath();
-      for (var i = 0; i < numberOfSingleDots; i++) {
-        ctx.arc(offset + 70.5 + i * 40, 55.5, 5, 0, Math.PI * 2, true);
-      }
-      ctx.fill();
-      ctx.beginPath();
-      ctx.arc(offset + 250.5, 35.5, 5, 0, Math.PI * 2, true);
-      ctx.arc(offset + 250.5, 75.5, 5, 0, Math.PI * 2, true);
-      ctx.fill();
-      offset = 240;
-    }
+  for (var i = 0; i < numberOfStrings; i++) {
+    draw.line(20, 25 + i * 20, 500, 25 + i * 20).stroke({
+      width: 1
+    });
   }
-}
+
+  for (var i = 0; i <= numberOfFrets; i++) {
+    draw.line(20 + i * 20, 25, 20 + i * 20, 85).stroke({
+      width: 1
+    });
+  }
+
+  var offset = 0;
+  for (var o = 0; o < numberOfOctaves; o++) {
+    for (var i = 0; i < numberOfSingleDots; i++) {
+      draw.circle(10).fill('#003333').move(offset + 65 + i * 40, 50)
+    }
+    draw.circle(10).fill('#003333').move(offset + 245, 30);
+    draw.circle(10).fill('#003333').move(offset + 245, 70);
+    offset = 240;
+  }
+})
